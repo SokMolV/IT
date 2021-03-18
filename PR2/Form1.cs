b9f0367ef47fc6b39b388c0bb5747a54b032ef3b
Mariya Sokolenko <marya.sokolenko@gmail.com>
2021-03-18 20:34:12 +0300

    12

--- PR2/Form1.cs
+++ PR2/Form1.cs
@@ -23,7 +23,6 @@ namespace PR2
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            textBox1.Text = " ";
             // Вывод строки в многострочный редактор
             textBox2.Text = "Практическая работа №2 УРАВНЕНИЕ 11 Соколенко М.В.";
             textBox2.Text += Environment.NewLine + "Рассчитать значение выражения y = корень(3 + lnx + 15 - x)/(1 + sin((2 + x*x)/1 +x)) ";
