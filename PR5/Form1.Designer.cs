0983a3baee1e1e4db311577c0b1ed69ec54d1483
Mariya Sokolenko <marya.sokolenko@gmail.com>
2021-04-04 08:56:36 +0300

    12

--- PR5/Form1.Designer.cs
+++ PR5/Form1.Designer.cs
@@ -31,15 +31,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(26, 303);
+            this.button1.Location = new System.Drawing.Point(26, 291);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(112, 37);
+            this.button1.Size = new System.Drawing.Size(118, 29);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Пуск";
+            this.button1.Text = "Подсчет";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -56,16 +57,32 @@
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Привет, меня зовут мария!",
+            "Я рада вас видеть здесь.",
+            "Как ваши дела?"});
             this.listBox1.Location = new System.Drawing.Point(26, 24);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(415, 244);
             this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 327);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Замена";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(615, 391);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
@@ -81,6 +98,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- PR5/Form1.cs
+++ PR5/Form1.cs
@@ -19,25 +19,48 @@ namespace PR5
 
         private void button1_Click(object sender, EventArgs e)
         {
+
             // Получаем номер выделенной строки     
-            int index = listBox1.SelectedIndex;     
+            int index = listBox1.SelectedIndex;
             // Считываем строку в перменную str     
-            string str = (string)listBox1.Items[index];     
+            string str = (string)listBox1.Items[index];
             // Узнаем количество символов в строке     
-            int len = str.Length;      
+            int len = str.Length;
             // Считаем, что количество пробелов равно 0     
-            int count = 0;      
+            int count = 0;
             // Устанавливаем счетчик символов в 0     
-            int i = 0;     
+            int i = 0;
             //Организуем цикл перебора всех символов в строке     
-            while (i < len - 1)     
-            {         
+            while (i < len - 1)
+            {
                 // Если нашли пробел, то увеличиваем счетчик пробелов на 1         
-                if (str[i] == 'а')             
-                    count++;         
-                    i++;     
-            }     
-            label1.Text = "Количество букв а = " + count.ToString(); 
+                if (str[i] == 'а')
+                    count++;
+                i++;
+            }
+            label1.Text = "Количество букв а = " + count.ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {    
+            int index = listBox1.SelectedIndex;    
+            string str = (string)listBox1.Items[index];  
+            int len = str.Length;   
+            int count = 0;   
+            int i = 0; 
+            while (i < len - 1)
+            {      
+                if (str[i] == 'а')
+                    count++;
+                i++;
+            }
+            str = str.Replace("а", "*");
+            label1.Text = " " + str.ToString();
         }
     }
 }
